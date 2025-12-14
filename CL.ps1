1012db2dc0ab3c8be3e41831e8a7cb40951cc097
Nohuto <noel.ballier@gmail.com>
2025-12-14 14:23:53 +0100

    localappdata

--- CL.ps1
+++ CL.ps1
@@ -1,5 +1,5 @@
-$dir = "$env:APPDATA\Noverse"
-$dest = "$env:APPDATA\Noverse\NVFetch.ps1"
+$dir = "$env:LOCALAPPDATA\Noverse"
+$dest = "$env:LOCALAPPDATA\Noverse\NVFetch.ps1"
 
 if (!(Test-Path $dir)) { ni -ItemType Directory -Path $dir -Force | Out-Null }
 mv "$env:USERPROFILE\Downloads\NVFetch.ps1" -Destination $dest -Force
@@ -8,5 +8,6 @@ if (!(Test-Path $profile)) { ni -ItemType File -Path $profile -Force | Out-Null 
 ac $profile -Value "`nfunction nvfetch { & `"$dest`" @args }"
 
 . $profile
+
 
 
